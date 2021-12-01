1709ae7955ec8d46119998b22438eb2bc10139cd
ssm-0123 <ssm-0123@users.noreply.github.com>
2021-12-01 14:52:15 -0800

    Everything good

--- gradetracker.py
+++ gradetracker.py
@@ -1,13 +1,52 @@
-filer = open('database.csv')
+import json
 
+filer = open('database.txt')
 Read = filer.read()
+sub = json.loads(Read)
 
-AA = Read.split('\n')
-subgrade = []
-subgrade.append(AA)
 
 def subname():
-    for i in subgrade:
-        print(subgrade[i])
+    x = 1
+    for i in sub:
+        print(x,end=". ")
+        for a in i:
+            print(a)
+        x = x+1
+    ask = input("Choose the number to change name of the subject: ")
+    now = int(ask)
+    askname = input("Enter new name of subject")
+    sub[now-1][0] = askname
 
-subname()
+def subgrade():
+    x = 1
+    for i in sub:
+        print(x,end=". ")
+        for a in i:
+            print(a)
+        x = x+1
+    ask = input("Choose the number to change grade of the subject: ")
+    now = int(ask)
+    askname = input("Enter new grade of subject")
+    sub[now-1][1] = askname
+    
+def savedata():
+    filew = open('database.txt','w')
+    filew.write(json.dumps(sub))
+
+
+def main():
+    while True:
+        print("1. Change subject name")
+        print("2. Change subject grade")
+        print("3. Save & Exit")
+        Main = input("")
+        if Main == "1":
+            subname()
+        elif Main == "2":
+            subgrade()
+        elif Main == "3":
+            savedata()
+            break
+
+
+main()
